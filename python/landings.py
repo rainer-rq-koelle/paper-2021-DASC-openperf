f8e38fe653d5ff03a3f1ed72329d4967bd4178ff
Enrico Spinielli <enrico.spinielli@gmail.com>
2021-07-13 12:02:36 +0200

    RWY and landing time

--- python/landings.py
+++ python/landings.py
@@ -3,23 +3,23 @@ import pandas as pd
 from pyhere import here, find_root
 import glob
 import os
-
-airport = "EGLL"
-date = "2019-03-01"
-i_file = here("data-raw", airport, f"{airport}_{date}_history.feather")
-o_file = here("data", airport, f"{airport}_{date}_arrivals.feather")
-
+import datetime
 
 for adsb_file in glob.iglob(str(here("data-raw")) + '/**/*.feather', recursive=True):
     airport, date, _ = os.path.basename(adsb_file).split('_')
+    ts = datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%S.%f")
+    print(f"Processing {airport} on {date}, {adsb_file}...{ts}")
     t = Traffic(pd.read_feather(adsb_file))
-    arrs = (t.landing_at('EGLL')
-        .next('aligned_on_EGLL')
+    arrs = (t.landing_at(airport)
+        .next(f'aligned_on_{airport}')
         .summary(['callsign', 'stop', 'ILS_max'])
         .to_csv(here("data", airport, f"{airport}_{date}_arrivals.csv"))
     )
+    ts = datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%S.%f")
+    # print(f"Processing {airport} on {date}, {adsb_file}...{ts}")
 
 
-# arrs = t.landing_at('EGLL').eval()
-# a = arrs.next('aligned_on_EGLL').eval()
-# a.summary(['callsign', 'stop', 'ILS_max']).to_csv("/Users/espin/repos/paper-2021-DASC-openperf/data/EGLL/EGLL_2019-03-01_arrivals.csv")
+# t = Traffic(pd.read_feather('/Users/espin/repos/paper-2021-DASC-openperf/data-raw/EHAM/EHAM_2019-03-01_history.feather'))
+# arrs = t.landing_at('EHAM').eval()
+# a = arrs.next('aligned_on_EHAM').eval()
+# a.summary(['callsign', 'stop', 'ILS_max']).to_csv("/Users/espin/repos/paper-2021-DASC-openperf/data/EHAM/EHAM_2019-03-01_arrivals_other.csv")
